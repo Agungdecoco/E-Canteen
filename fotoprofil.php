5b6f8549fc2cfcff17ad54b35073cff984868a54
NadiatuWafrina <nadiawafrina99@gmail.com>
2020-10-30 21:00:36 +0700

    login

---
+++ fotoprofil.php
@@ -0,0 +1,24 @@
+<?php
+class fotoprofil{
+private $uploadphoto;
+private $inputsize;
+
+public function __construct($uploadphoto, $inputsize){
+	$this->uploadphoto = $uploadphoto;
+	$this->inputsize = $inputsize;
+}
+public function upload(){
+	if($this->inputsize < 500000){
+		echo "successful!";
+	}else{
+		echo "failed!";
+	}
+}
+}
+
+$uploadphoto = "ABC";
+$inputsize = "5000";
+
+$profil = new profil($uploadphoto, $inputsize);
+$profil->upload();
+?>
