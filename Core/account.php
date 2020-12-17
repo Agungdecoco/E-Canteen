6b0a3fc625e0b93de100e1512d8adcf27fe7144e
Amalia-Salsabilla <amalia.salsa28@gmail.com>
2020-12-17 10:54:28 +0700

    account

---
+++ Core/account.php
@@ -0,0 +1,68 @@
+<?php
+
+class account{
+
+	private $id;
+	private $name;
+	private $email;
+	private $password;
+	private $saldo;
+
+public function getid()
+{
+	return $this->id;
+}
+public function getname()
+{
+	return $this->name;
+}
+public function getemail()
+{
+	return $this->email;
+}
+public function getpassword()
+{
+	return $this->password;
+}
+public function setid()
+{
+	$this->id = $id;
+}
+public function setname()
+{
+	$this->name = $name;
+}
+public function setemail()
+{
+	$this->email = $email;
+}
+public function setpassword()
+{
+	$this->password = $password;
+}
+public function save()
+{
+	if ($this->isSave()){
+		echo "Account already!";
+	} else {
+		echo "Account not ready!";
+	}
+}
+public function delete()
+{
+	if ($this->isdelete()){
+		echo "Account has been delete.";
+	} else {
+		echo "Delete failed.";
+	}
+}
+public function setsaldo()
+{
+	$this->saldo = $saldo;
+}
+public function getsaldo()
+{
+	return $this->saldo;
+}
+}
+
